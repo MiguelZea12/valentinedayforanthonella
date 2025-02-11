cb334660814c50ab46afee259969d6cd6b9ce569
MiguelZea12 <aroryt02@gmail.com>
2025-02-11 04:28:10 -0500

    funciona

--- src/components/SectionOne.tsx
+++ src/components/SectionOne.tsx
@@ -5,13 +5,12 @@ import { Navigation, Pagination, EffectCoverflow } from 'swiper/modules';
 
 
 const images = [
-  "public/img1",
-  "public/img2.jpg",
-  "public/img3.jpg",
-  "public/img4.jpg",
-  "public/img5.jpg",
-  "public/img6.jpg",
-  "src/assets/img/cc.png",
+  "/img1.jpg",
+  "/img2.jpg",
+  "/img3.jpg",
+  "/img4.jpg",
+  "/img5.jpg",
+  "/img6.jpg",
 ];
 
 const SectionOne: React.FC = () => {
